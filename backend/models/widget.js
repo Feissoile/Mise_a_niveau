d076205018f72817a0da33824617c2d1c09ad57c
Feissoile <feissoileali@gmail.com>
2024-11-15 10:02:20 +0100

    widgetSchema

--- backend/models/widget.js
+++ backend/models/widget.js
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose")
-
-
+const mongoose = require("mongoose");
 
 const widgetSchema = mongoose.Schema({
-
+  name: String,
+  description: String,
+  category: String, 
+  status: String,
+  createdAt: Date,
+  updatedAt: Date,
+  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
-const Widget = mongoose.model("widgets", widgetSchema)
+const Widget = mongoose.model("widgets", widgetSchema);
 
-module.exports = Widget
+module.exports = Widget;
