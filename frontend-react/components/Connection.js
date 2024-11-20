808db0f63a71bb882df8d1e06d022feda1c5b0ea
Feissoile <feissoileali@gmail.com>
2024-11-20 19:52:53 +0100

    test

--- frontend-react/components/Connection.js
+++ frontend-react/components/Connection.js
@@ -1,12 +1,44 @@
 import React, { useState, useEffect } from "react";
+import {Alert,
+    StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  
+} from "react"
 
+const BACKEND_ADDRESS = "http://localhost:3000";
 
 function Connection() {
-    return (
-      <div>
-      
-      </div>
-    );
-   }
-   
-   export default Connection;
+  const [signInUsermail, setSignInUsermail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
+
+  const handleConnection = () => {
+    console.log({ email: signInUsermail, password: "********" }); // Cacher le mot de passe dans les logs
+    fetch(`${BACKEND_ADDRESS}/users/signin`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: signInUsermail, password: signInPassword }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.result) {
+          //dispatch(login({ email: signInUsermail, token: data.token }));
+          setSignInUsermail("");
+          setSignInPassword("");
+        } else {
+          Alert.alert("Email et/ou mot de passe incorrect(s).");
+        }
+      });
+  };
+
+  return <div></div>;
+}
+
+export default Connection;
