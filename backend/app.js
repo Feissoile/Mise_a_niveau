5958d63558e2e5426b398805537bf7aaf5413ef2
Feissoile <feissoileali@gmail.com>
2024-11-20 13:48:53 +0100

    react

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,8 @@ var app = express();
 
 //app.use(cors(corsOptions))
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- frontend-react/components/Home.js
+++ frontend-react/components/Home.js
@@ -1,13 +1,11 @@
 import styles from '../styles/Home.module.css';
+import Connection from './Connection';
+import React from 'react';
 
 function Home() {
  return (
    <div>
-     <main className={styles.main}>
-       <h1 className={styles.title}>
-         Welcome to <a href="https://nextjs.org">Next.js!</a>
-       </h1>
-     </main>
+     <Connection />
    </div>
  );
 }
